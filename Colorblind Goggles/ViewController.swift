c7cfcaff7d99bfc05638fac6a550d0a1d7ccb2b3
Edmund Dipple <elmundio1987@gmail.com>
2015-11-27 15:46:45 +0000

    Very slow, but working camera

--- Colorblind Goggles/ViewController.swift
+++ Colorblind Goggles/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
+import AssetsLibrary
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    let cbf = ColourblindFilter()
+    var timer: NSTimer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.configureCamera()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         filterImage()
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1/30, target: self, selector: "takePict:", userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +51,63 @@ class ViewController: UIViewController {
             return (UInt8(r),UInt8(g),UInt8(b),255)
             
         })
+    }
+    
+    func configureCamera(){
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+        // as the media type parameter.
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+        
+        var input: AnyObject!
+        do{
+            try input = AVCaptureDeviceInput(device: captureDevice)
+        }catch let error as NSError {
+            // If any error occurs, simply log the description of it and don't continue any more.
+            print("\(error.localizedDescription)")
+            return
+        }
+        
+        self.stillImageOutput = AVCaptureStillImageOutput()
+        
+        // Initialize the captureSession object.
+        captureSession = AVCaptureSession()
+        // Set the input device on the capture session.
+        captureSession?.addInput(input as! AVCaptureInput)
+        captureSession?.addOutput(self.stillImageOutput)
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        view.bringSubviewToFront(imageView)
+        
+        
+        captureSession?.startRunning()
+      
+    }
+    
+    func takePict(timer: NSTimer){
+        
+        var layer : AVCaptureVideoPreviewLayer = videoPreviewLayer! as AVCaptureVideoPreviewLayer
+        
+        self.stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo).videoOrientation = layer.connection.videoOrientation
+        
+        self.stillImageOutput?.captureStillImageAsynchronouslyFromConnection(self.stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo), completionHandler: {
+            (imageDataSampleBuffer,error) -> Void in
+            if ((imageDataSampleBuffer) != nil) {
+                var imageData : NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                var image : UIImage = UIImage(data: imageData)!
+                self.imageView.image = image.applyOnPixels({ (point, redColor, greenColor, blueColor, alphaValue) -> (newRedColor: UInt8, newgreenColor: UInt8, newblueColor: UInt8, newalphaValue: UInt8) in
+                    let (r,g,b,a) = self.cbf.FilterColour(R: Double(redColor), G: Double(greenColor), B: Double(blueColor), A: Double(alphaValue), f: ColourblindFilter.FilterType.Deuteranopia)
+                    return (UInt8(r),UInt8(g),UInt8(b),255)
+                    
+                })
+            }
+        })
+        
     }
     
     private func createARGBBitmapContext(inImage: CGImageRef) -> CGContext {

27e3730b40a0849d35ef8e7cc81f2ef6b8fd77fe
Edmund Dipple <elmundio1987@gmail.com>
2015-12-01 22:08:46 +0000

    Add some information. Open links in external browser.

--- Colorblind Goggles/AboutController.swift
+++ Colorblind Goggles/AboutController.swift
@@ -8,9 +8,34 @@
 
 import UIKit
 
-class AboutController: UIViewController  {
+class AboutController: UIViewController,UIWebViewDelegate  {
+    
+    @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var closeButton: UIBarButtonItem!
     @IBAction func clickedCloseButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: {})
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.delegate = self
+        loadAddressURL()
+    }
+    
+    func loadAddressURL(){
+        if let url = NSBundle.mainBundle().URLForResource("info", withExtension: "html") {
+            webView.loadRequest(NSURLRequest(URL: url))
+        }
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        let url: NSURL = request.URL!
+        let isExternalLink: Bool = url.scheme == "http" || url.scheme == "https" || url.scheme == "mailto"
+        if (isExternalLink && navigationType == UIWebViewNavigationType.LinkClicked) {
+            return !UIApplication.sharedApplication().openURL(request.URL!)
+        } else {
+            return true
+        }
     }
 }
